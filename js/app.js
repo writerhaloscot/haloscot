eef2358c19c62614c1922fd9e084abfe280a8b4f
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2022-08-01 13:57:09 -0400

    reorganize website

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function () {
     var m = '<meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="UTF-8">';
     var s = '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Text:wght@700&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" crossorigin="anonymous">';
     var v = 'img/favicon.png';
-    var c = 'css/style.css?v=1.23';
+    var c = 'css/style.css?v=1.24';
     var h = 'includes/header.html';
     var f = 'includes/footer.html';
 
