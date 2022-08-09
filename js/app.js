c922f09da2dd3e12defcb26421281f19787e2eee
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2022-08-08 20:37:00 -0400

    more press update

--- js/app.js
+++ js/app.js
@@ -48,6 +48,7 @@ $(function () {
         ['Best Thriller Books: Kashif Hussain Review', 'https://www.bestthrillerbooks.com/kashif-hussain/i-will-kill-you-by-halo-scot'],
         ['Power Librarian Book Reviews', 'https://powerlibrarian.wordpress.com/2022/07/26/i-will-kill-you/'],
         ['GBHBL Horror Book Reviews', 'https://www.gbhbl.com/horror-book-review-i-will-kill-you-halo-scot/'],
+        ['Morgan K Tanner: Book Review', 'https://morganktanner.com/2022/08/08/book-review-i-will-kill-you-by-halo-scot/'],
         ['The Heartbeat of a Million Dreams'],
         ['A.J. Calvin', 'https://ajcalvin.net/book-review-the-heartbeat-of-a-million-dreams-by-halo-scot/'],
         ['Jude in the Stars', 'https://judeinthestars.com/2022/07/28/the-heartbeat-of-a-million-dreams-halo-scot/'],
