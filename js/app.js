9ab5eeeb4d54bb73d70a63ec104f724d662566b0
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2022-09-01 15:29:35 -0400

    removed font awesome for speed & used SVGs

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function () {
 
     // LOAD INCLUDES
     /* var m = '<meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="UTF-8">';
-    var s = '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Text:wght@700&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" crossorigin="anonymous">'; 
+    var s = '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Text:wght@700&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">'; 
     var v = 'img/favicon.png';
     var c = 'css/style.css?v=1.24';*/
     var h = 'includes/header.html';
