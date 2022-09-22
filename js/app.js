15c30ee0c1ddca2ca68505f541f80608a2ac1d45
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2022-09-21 21:25:43 -0400

    clear cache

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function () {
     /* var m = '<meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="UTF-8">';
     var s = '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Text:wght@700&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">'; 
     var v = 'img/favicon.png';
-    var c = 'css/style.css?v=1.24';*/
+    var c = 'css/style.css?v=1.25';*/
     var h = 'includes/header.html';
     var f = 'includes/footer.html';
 
